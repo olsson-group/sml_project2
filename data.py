b3eb05489b3f5f5baf5e57bb95d46d2034857dca
Mathias Schreiner <matschreiner@gmail.com>
2024-09-25 16:36:45 +0200

    get data

--- data.py
+++ data.py
@@ -1,10 +1,10 @@
 import torch_geometric as geom
 
 
-def download_data(molecule):
-    geom.datasets.MD17(root="data/MD17", name=molecule)
+def get_data(molecule):
+    return geom.datasets.MD17(root="data/MD17", name=molecule)
 
 
 def download_all():
     for molecule in ["benzene", "malonaldehyde", "ethanol", "toluene"]:
-        download_data(molecule)
+        get_data(molecule)
